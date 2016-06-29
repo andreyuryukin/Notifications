d0ffc93be73549f4b486c36859c2469f3d6df7ab
andreyuryukin <andrey.uryukin@gmail.com>
2016-06-29 15:25:52 +0300

    commit

--- app/src/main/java/com/example/desktop/notifications/NotificationService.java
+++ app/src/main/java/com/example/desktop/notifications/NotificationService.java
@@ -40,7 +40,13 @@ public class NotificationService extends Service {
     public boolean receiverRegistered;
     public static final int ACT_CONNECTED = 1;
     public static final int ACT_DISCONNECTED = 2;
+    public static final int ACT_RECORDING_STARTED = 3;
+    public static final int ACT_RECORDING_FINISHED = 4;
     public int notificationNumber;
+    public String contentTitle = "contentTitle";
+    public String contentText = "contentText";
+    public Intent notificationIntent;
+    public PendingIntent pendingIntent;
 
     @Nullable
     @Override
@@ -51,7 +57,7 @@ public class NotificationService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.v("onCreate", "Start");
+        Log.v("**onCreate**", "Start");
 
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
@@ -61,18 +67,18 @@ public class NotificationService extends Service {
                 String action = intent.getAction();
                 device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
 
-                Log.v("Receiver", action);
+                Log.v("**onCreate**", action);
 
                 if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)) {
-                    Log.v("Receiver", "ACTION_ACL_CONNECTED " + device.getName() + "," + device.getAddress() + "\n");
-                    sendNotificationBT(device.getName(), ACT_CONNECTED);
-                    startRecording();
+                    Log.v("**onCreate**", "ACTION_ACL_CONNECTED " + device.getName() + "\n");
+                    sendNotification(ACT_CONNECTED, file);
+                    recordAudioFragment();
                 } else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
-                    Log.v("Receiver", "ACTION_ACL_DISCONNECTED " + device.getName() + "," + device.getAddress() + "\n");
-                    sendNotificationBT(device.getName(), ACT_DISCONNECTED);
-                    startRecording();
-//                } else if (Intent.ACTION_POWER_CONNECTED.equals(action)) {
-//                    startRecording();
+                    Log.v("**onCreate**", "ACTION_ACL_DISCONNECTED " + device.getName() + "\n");
+                    sendNotification(ACT_DISCONNECTED, file);
+                    recordAudioFragment();
+                } else if (Intent.ACTION_POWER_CONNECTED.equals(action)) {
+                    recordAudioFragment();
                 }
             }
         };
@@ -80,16 +86,14 @@ public class NotificationService extends Service {
         IntentFilter filter1 = new IntentFilter(BluetoothDevice.ACTION_ACL_CONNECTED);
         IntentFilter filter2 = new IntentFilter(BluetoothDevice.ACTION_ACL_DISCONNECTED);
         IntentFilter filter3 = new IntentFilter(Intent.ACTION_POWER_CONNECTED);
-        IntentFilter filter4 = new IntentFilter(Intent.ACTION_POWER_DISCONNECTED);
 
         this.registerReceiver(mReceiver, filter1);
         this.registerReceiver(mReceiver, filter2);
         this.registerReceiver(mReceiver, filter3);
-        this.registerReceiver(mReceiver, filter4);
 
         receiverRegistered = true;
 
-        Log.v("onCreate", "Registering filters");
+        Log.v("**onCreate**", "Registering filters");
 
     }
 
@@ -109,85 +113,56 @@ public class NotificationService extends Service {
                 this.unregisterReceiver(mReceiver);
                 receiverRegistered = false;
             }
-
             Log.v("**NotificationService**", "onDestroy");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void sendNotification(String action, Integer actionCode, File file) {
+    public void sendNotification(Integer actionCode, File file) {
 
         Calendar c = Calendar.getInstance();
 
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
         String formattedDate = df.format(c.getTime());
 
         context = getApplicationContext();
 
-        if (actionCode == 0) {
-            Intent notificationIntent = new Intent(Intent.ACTION_VIEW);
-            Uri uri = Uri.parse("file://" + file.getAbsolutePath());
-            notificationIntent.setDataAndType(uri, "audio/*");
-            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-            builder = new Notification.Builder(context)
-                    .setContentTitle(formattedDate)
-                    .setContentText(file.getAbsolutePath())
-                    .setDefaults(Notification.DEFAULT_SOUND)
-                    .setAutoCancel(true)
-                    .setSmallIcon(R.drawable.ic_notification)
-                    .setContentIntent(pendingIntent);
-        } else {
-            builder = new Notification.Builder(context)
-                    .setContentTitle(formattedDate)
-                    .setContentText(action)
-                    .setDefaults(Notification.DEFAULT_SOUND)
-                    .setAutoCancel(true)
-                    .setSmallIcon(R.drawable.ic_notification);
-        }
-
-        notification = builder.build();
-
-        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationNumber = generateRandom();
-        notificationManager.notify(notificationNumber, notification);
-    }
-
-    public void sendNotificationBT(String deviceName, int actionDone) {
-
-        String title = "Bluetooth Device";
-
-        switch (actionDone) {
+        switch (actionCode) {
             case ACT_CONNECTED:
-                title = "Bluetooth Device Connected";
-                break;
+                contentTitle = "Bluetooth Device Connected";
+                contentText = formattedDate + " " + device.getName();
             case ACT_DISCONNECTED:
-                title = "Bluetooth Device Disconnected";
-                break;
+                contentTitle = "Bluetooth Device Disconnected";
+                contentText = formattedDate + " " + device.getName();
+            case ACT_RECORDING_STARTED:
+                contentTitle = "Recording Started";
+                contentText = formattedDate;
+            case ACT_RECORDING_FINISHED:
+                contentTitle = "Recording Finished";
+                contentText = file.getAbsolutePath();
+
+                notificationIntent = new Intent(Intent.ACTION_VIEW);
+                Uri uri = Uri.parse("file://" + file.getAbsolutePath());
+                notificationIntent.setDataAndType(uri, "audio/*");
+                pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         }
 
-        Calendar c = Calendar.getInstance();
-
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String contentText = df.format(c.getTime()) + " " + deviceName;
-
-        context = getApplicationContext();
         builder = new Notification.Builder(context)
-                .setContentTitle(title)
+                .setContentTitle(contentTitle)
                 .setContentText(contentText)
                 .setDefaults(Notification.DEFAULT_SOUND)
                 .setAutoCancel(true)
-                .setSmallIcon(R.drawable.ic_notification);
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentIntent(pendingIntent);
 
         notification = builder.build();
-
         notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationNumber = generateRandom();
         notificationManager.notify(notificationNumber, notification);
     }
 
-    public void startRecording() {
+     public void recordAudioFragment() {
 
         new Timer().schedule(new TimerTask() {
             @Override
@@ -195,39 +170,37 @@ public class NotificationService extends Service {
                 mediaRecorder.stop();
                 mediaRecorder.reset();
                 mediaRecorder.release();
-                Log.v("startRecording", "Stopping voice recording");
-                sendNotification("Stopping voice recording ...", 0, file);
+                Log.v("**recordAudioFragment**", "Stopping voice recording");
+                sendNotification(ACT_RECORDING_FINISHED, file);
             }
         }, 10000);
 
         try {
             Calendar c = Calendar.getInstance();
 
-            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
 
             file = new File(Environment.getExternalStorageDirectory(),
                     "" + df.format(c.getTime()) + ".3gp");
 
             mediaRecorder = new MediaRecorder();
             mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-            mediaRecorder
-                    .setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-            mediaRecorder
-                    .setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
             mediaRecorder.setOutputFile(file.getAbsolutePath());
 
-            Log.v("startRecording", "Starting voice recording");
+            Log.v("**recordAudioFragment**", "Starting voice recording");
 
             mediaRecorder.prepare();
             mediaRecorder.start();
-            sendNotification("Starting voice recording ...", 1, file);
+            sendNotification(ACT_RECORDING_STARTED, file);
 
         } catch (IllegalStateException | IOException e) {
             e.printStackTrace();
         }
     }
 
-    public int generateRandom(){
+    public int generateRandom() {
         Random random = new Random();
         return random.nextInt(9999 - 1000) + 1000;
     }
